31ca5c3aaa0b92a0eb30318fdba941287e2d5fdf
NoOne1591 <mohitkhicher1591@gmail.com>
2026-06-18 16:33:45 +0530

    feat: integrate Appwrite authentication and add UI components for header, footer, and buttons

---
+++ 07-mainproject/mainproject/src/components/index.js
@@ -0,0 +1,12 @@
+import Footer from "./Footer/Footer";
+import Header from "./Header/Header";
+import Container from "./container/container";
+import Logo from "./logo";
+import LogoutBtn from "./Header/LogoutBtn";
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn
+}
